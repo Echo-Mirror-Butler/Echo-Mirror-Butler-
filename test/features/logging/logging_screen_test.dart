3119c738b2ffda78d9d4a317f21c0564e552424f
sendi0011 <johnsendi727@gmail.com>
2026-05-29 16:13:41 +0100

    revert: super parameter change in logging_screen_test.dart
    - Revert to original constructor to fix test failures
    - Will address analyzer warning separately if needed

--- test/features/logging/logging_screen_test.dart
+++ test/features/logging/logging_screen_test.dart
@@ -30,7 +30,8 @@ class _FakeLoggingNotifier extends LoggingNotifier {
 }
 
 class _FakeAuthNotifier extends AuthNotifier {
-  _FakeAuthNotifier._(super.repo, AuthState initialState) {
+  _FakeAuthNotifier._(MockAuthRepository repo, AuthState initialState)
+      : super(repo) {
     state = initialState;
   }
 
